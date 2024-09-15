dfce90d3c82df646ba33df29582c666faf97c136
Riccardo <r.d.b.97@live.it>
2024-09-15 18:36:51 +0200

    Feature: Add Material Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MaterialModule } from './shared/modules/material/material.module';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MaterialModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'TrainApp';
+
+  isUserLogged: boolean = true;
+  login(){
+    this.isUserLogged = true;
+  }
+
+  logout(){
+    this.isUserLogged = false;
+  }
 }
